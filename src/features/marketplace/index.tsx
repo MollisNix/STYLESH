91c484fdb18f33785e7a25482fb258633618c80a
Mollis_Nix <vadim.shebela@gmail.com>
2025-04-12 14:01:09 +0200

    Search method was implemented in marketplace feature. Zustand Store needs a refactor!!!

--- src/features/marketplace/index.tsx
+++ src/features/marketplace/index.tsx
@@ -1,61 +1,38 @@
 "use client";
 
-import { SquarePlus, Heart } from "lucide-react";
-
-import Image from "next/image";
 import { Input } from "../../components/ui/input";
-
-import {
-	Card,
-	CardContent,
-	CardFooter,
-	CardHeader,
-} from "../../components/ui/card";
 
 import { Skeleton } from "@/components/ui/skeleton";
 
-import { map } from "lodash";
 import { useEffect } from "react";
 
-import { useMarketplaceStore } from "../../../store/marketplace-store";
+import {
+	useMarketplaceStore,
+} from "../../../store/marketplace-store";
+import {
+	MarketplaceItems,
+	SearchedItems,
+} from "./components/marketplace-items";
 
 export const Marketplace = () => {
 	const marketplaceData = useMarketplaceStore(
 		(state) => state.marketplaceItems
 	);
-	const updatedLikedItems = useMarketplaceStore(
-		(state) => state.updatedLikedItems
-	);
 	const updateMarketplace = useMarketplaceStore(
 		(state) => state.updateMarketplace
 	);
-
-	const updateInCartItems = useMarketplaceStore(
-		(state) => state.updateInCartItems
+	const updateSearchedItems = useMarketplaceStore(
+		(state) => state.updateSearchedItems
 	);
+	const searchedItems = useMarketplaceStore((state) => state.searchedItems);
+
+	const onChangeHandler: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+		updateSearchedItems(e.target.value.toLowerCase());
+	};
 
 	useEffect(() => {
 		updateMarketplace();
 	}, []);
-
-	const addTowishListHandler: React.MouseEventHandler<SVGSVGElement> = (e) => {
-		const target = e.currentTarget;
-		marketplaceData.map((item) => {
-			if (item.id === target.id) {
-				updatedLikedItems(target.id, item);
-			}
-		});
-	};
-
-	const addToCartHandler: React.MouseEventHandler<SVGSVGElement> = (e) => {
-		const target = e.currentTarget;
-
-		marketplaceData.map((item) => {
-			if (item.id === target.id) {
-				updateInCartItems(target.id, item);
-			}
-		});
-	};
 
 	if (marketplaceData.length === 0) {
 		return (
@@ -80,68 +57,21 @@ export const Marketplace = () => {
 		);
 	}
 
-return (
-<div className="container mx-auto my-10">
-	<div className="top-content flex justify-between text-2xl mb-10">
-		<h2>Весь товар</h2>
-		<form action="get">
-			<Input
-				placeholder="Знайди свої кросівки!"
-				className="w-2xl rounded-sm"
-			/>
-		</form>
-	</div>
-	<div className="marketplace flex flex-wrap justify-center gap-5">
-		{map(marketplaceData, (item) => (
-			<Card key={item.id} className="basis-2xs">
-				<CardHeader>
-					{item.isLiked ? (
-						<Heart
-							className="cursor-pointer"
-							color="#e60a20"
-							onClick={addTowishListHandler}
-							id={item.id}
-						/>
-					) : (
-						<Heart
-							className="cursor-pointer"
-							onClick={addTowishListHandler}
-							id={item.id}
-						/>
-					)}
-
-					<Image
-						src={item.itemImage}
-						alt="marketplace-item-image"
-						width={286}
-						height={100}
-						className="select-none"
+	return (
+		<div className="container mx-auto my-10">
+			<div className="top-content flex justify-between text-2xl mb-10">
+				<h2>Весь товар</h2>
+				<form action="get">
+					<Input
+						placeholder="Знайди свої кросівки!"
+						className="w-2xl rounded-sm"
+						onChange={onChangeHandler}
 					/>
-				</CardHeader>
-				<CardContent>
-					<strong>{item.itemName}</strong>
-				</CardContent>
-				<CardFooter className="flex justify-between">
-					<strong>Ціна:</strong>
-					<strong>{item.itemPrice} тис. грн.</strong>
-					{item.isAdded ? (
-						<SquarePlus
-							className="cursor-pointer"
-							color="#07cf43"
-							onClick={addToCartHandler}
-							id={item.id}
-						/>
-					) : (
-						<SquarePlus
-							className="cursor-pointer"
-							onClick={addToCartHandler}
-							id={item.id}
-						/>
-					)}
-				</CardFooter>
-			</Card>
-		))}
-	</div>
-</div>
-);
+				</form>
+			</div>
+			<div className="marketplace flex flex-wrap gap-5">
+				{searchedItems.length === 0 ? <MarketplaceItems /> : <SearchedItems />}
+			</div>
+		</div>
+	);
 };
